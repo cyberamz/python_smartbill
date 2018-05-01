9e11703bb370c148c1b156262ea7c7bde71bf471
Dragos Ekart <office@cyberamz.com>
2018-05-01 16:40:01 +0300

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
      name='smartbill',
-     version='0.2',
+     version='0.2.1',
      author='CyberAmz.com',
      author_email='office@cyberamz.com',
      classifiers=[
