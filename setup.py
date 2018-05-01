7947545418fcee96a39ef1c09b5484cffd27de0f
Dragos Ekart <office@cyberamz.com>
2018-05-01 21:22:44 +0300

    finish version beta

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
      name='smartbill',
-     version='0.2.1',
+     version='0.2.2',
      author='CyberAmz.com',
      author_email='office@cyberamz.com',
      classifiers=[
